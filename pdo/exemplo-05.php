4ac1bcaa6e1bc299140f5042fd2ef942798fc1a7
Brenon Paul <bp.brenon@gmail.com>
2020-03-03 22:46:52 -0300

    DAO

---
+++ pdo/exemplo-05.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = :ID");
+
+$id = 2;
+
+
+$stmt->bindParam(":ID", $id);
+
+$stmt->execute();
+
+
+echo "Deletado com sucesso";
+
+?>
